62001b080e757070764c1c5caaf8ad629eaa2b21
gabribribribri <fuelanemo149@gmail.com>
2024-05-19 21:27:18 +0200

    implemented clap ! this thing is awesome

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
 //be means backend
 mod bigdecimal_be;
 mod f64_be;
+mod rug_be;
 mod rust_decimal_be;
 mod utils;
 
-fn main() {
-    let back_end = utils::parse_userargs::<String>(1).unwrap();
-    let max = utils::parse_userargs::<usize>(2).unwrap();
-    let jobs = utils::parse_userargs::<usize>(3).unwrap();
+use clap::Parser;
 
-    match back_end.as_str() {
-        "rust_decimal" => rust_decimal_be::run(max, jobs),
-        "bigdecimal" => bigdecimal_be::run(max, jobs),
-        "f64" => f64_be::run(max, jobs),
-        _ => utils::print_help_and_exit(0),
+#[derive(Parser, Debug)]
+#[command(about, long_about = None)]
+struct Args {
+    #[arg(short, long, help = "The backend library that will be used", value_parser = ["rust_decimal", "bigdecimal", "rug", "f64"])]
+    back_end: String,
+
+    #[arg(short, long, default_value_t = 1_000_000)]
+    iterations: usize,
+
+    #[arg(short, long, default_value_t = 1)]
+    jobs: usize,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.back_end.as_str() {
+        "rust_decimal" => rust_decimal_be::run(args.iterations, args.jobs),
+        "bigdecimal" => bigdecimal_be::run(args.iterations, args.jobs),
+        "f64" => f64_be::run(args.iterations, args.jobs),
+        "rug" => rug_be::run(args.iterations, args.jobs),
+        _ => (),
     }
+
+    // match back_end.as_str() {
+    //     "rust_decimal" => rust_decimal_be::run(max, jobs),
+    //     "bigdecimal" => bigdecimal_be::run(max, jobs),
+    //     "f64" => f64_be::run(max, jobs),
+    //     _ => utils::print_help_and_exit(0),
+    // }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,40 +1,4 @@
-use std::{fmt::Display, str::FromStr, time::Duration};
-
-pub fn print_help_and_exit(exit_code: i32) {
-    println!(
-        "
-A simple pi approximator
-How to use:
-    <back_end> <iterations> <jobs>
-
-    <back_end> choose between:
-        - f64 - Fastest but not precise
-        - rust_decimal - Precise and fast
-        - rug - STILL IN DEV
-        - bigdecimal - Very precise and very slow
-    "
-    );
-    std::process::exit(exit_code);
-}
-
-pub fn parse_userargs<T>(n: usize) -> Result<T, ()>
-where
-    T: FromStr,
-{
-    match std::env::args().nth(n) {
-        Some(arg) => match arg.parse::<T>() {
-            Ok(r) => Ok(r),
-            Err(_) => {
-                print_help_and_exit(2);
-                Err(())
-            }
-        },
-        None => {
-            print_help_and_exit(1);
-            Err(())
-        }
-    }
-}
+use std::{fmt::Display, time::Duration};
 
 pub fn result_message<T>(strategy: &str, max: usize, jobs: usize, time: Duration, piaprox: T)
 where
